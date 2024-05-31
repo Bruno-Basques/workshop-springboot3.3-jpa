d6bf7cae638736f4df3ec10c7a8e1790c60ac7b5
Bruno-Basques <bruno_basques@outlook.com>
2024-05-30 23:15:50 -0300

    Category

--- src/main/java/com/brunobasques/springboot_course/resources/OrderResource.java
+++ src/main/java/com/brunobasques/springboot_course/resources/OrderResource.java
@@ -29,7 +29,7 @@ public class OrderResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Order> findById(@PathVariable Long id)
 	{		
-		Order user = userService.findById(id);
-		return ResponseEntity.ok().body(user);
+		Order order = userService.findById(id);
+		return ResponseEntity.ok().body(order);
 	}
 }
---
+++ src/main/java/com/brunobasques/springboot_course/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.brunobasques.springboot_course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunobasques.springboot_course.entities.Category;
+import com.brunobasques.springboot_course.entities.Order;
+import com.brunobasques.springboot_course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll()
+	{
+		return categoryRepository.findAll();		
+	}
+	
+	public Category findById(Long id)
+	{
+		Optional<Category> category = categoryRepository.findById(id);		
+		return category.get();
+	}
+}
