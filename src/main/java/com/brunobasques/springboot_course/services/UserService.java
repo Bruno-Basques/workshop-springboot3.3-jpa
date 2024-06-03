6256d70a80f58811a341bda375cdccb133683954
Bruno-Basques <bruno_basques@outlook.com>
2024-06-03 20:01:10 -0300

    Exception handling - delete

--- src/main/java/com/brunobasques/springboot_course/services/UserService.java
+++ src/main/java/com/brunobasques/springboot_course/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.brunobasques.springboot_course.entities.User;
@@ -34,7 +36,19 @@ public class UserService {
 	
 	public void delete(Long id)
 	{
+		try 
+		{
 		userRepository.deleteById(id);
+		} 
+		catch(EmptyResultDataAccessException e)
+		{
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e)
+		{
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public User update(Long id, User user)
---
+++ src/main/java/com/brunobasques/springboot_course/services/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package com.brunobasques.springboot_course.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg)
+	{
+		super(msg);
+	}
+}
